c796a9e7d2c388b701cc80aab923adb7b1bb3ebd
python-fuse <hauwamuktar880@gmail.com>
2023-09-09 22:21:21 +0100

    added comments and used dict instead of lists

---
+++ README.md
@@ -0,0 +1,43 @@
+# Internship Tasks
+
+Welcome to my CodeAlpha internship tasks repository..
+
+## Task 1: [Python Password Generator]
+
+**Description:** [Generating strong Passwords in python]
+
+**Status:** [Completed]
+
+**Related Files:** [master/Task 1/password_generator_.py]
+
+**Additional Notes:** [I also made a basic GUI version of it in Tkinter.]
+
+## Task 2: [Quiz program]
+
+**Description:** [A quiz program in python where admins can add quizzes while the players play it.]
+
+**Status:** [Completed]
+
+**Related Files:** [master/Task 2/quiz_app.py]
+
+**Additional Notes:** [Play andhave fun!]
+
+## Task 3: [Image to sound using Tesseract OCR via pytesseract]
+
+**Description:** [In this python program,we take in an image containing text in it and feed it to pytesseract which in turn extracts the text and print/store in a variable.
+The variable is now read out using pytts3(a text to speech module) andalso saved as an mp3 file.]
+
+**Status:** Completed
+**Related Files:** master/Task_3/image_to_text.py
+
+**Additional Notes:** This is my most favorite task of the 3 tasks,I got to explore and learn about a couple of modules and libraries.Imean it's really amazing seeing and image "converted" to audio, right?
+
+
+## How to Use This Repository
+
+If you need to access any specific task or related files, navigate to the respective directory or file mentioned above.
+
+All required modules are available inside the requirements.txt file.
+
+Feel free to reach out if you have any questions or need further information about any of the tasks.
+
--- Task 1/password_generator_.py
+++ Task 1/password_generator_.py
@@ -1,25 +1,27 @@
-import string
-import random
+# Import necessary libraries
+import string   # Import the 'string' module for character sets
+import random   # Import the 'random' module for randomization
 
-letters = string.ascii_letters
-symbols = "()!@#$%_+-"
-digits = string.digits
-characters = symbols + letters + digits
-check = True
+# Define character sets for generating passwords
+letters = string.ascii_letters  # Uppercase and lowercase letters
+symbols = "()!@#$%_+-"         # Common symbols for passwords
+digits = string.digits          # Digits (0-9)
+characters = symbols + letters + digits  # Combine all characters into one list
 
-password = ""
+check = True   # Flag for user input validation
+password = ""  # Initialize an empty password string
 
-
+# Function to generate a random password of a specified length
 def generate_password(length):
     return password.join(random.sample(characters, length))
 
-
+# Loop to repeatedly prompt the user for password length until valid input is provided
 while check:
     user_input = input("Enter password length: ")
 
-    if user_input.isdigit():
-        result = generate_password(int(user_input))
-        print(f"\nYour Password is: {result}")
-        check = False
+    if user_input.isdigit():  # Check if the input is a digit
+        result = generate_password(int(user_input))  # Generate the password
+        print(f"\nYour Password is: {result}")  # Print the generated password
+        check = False  # Exit the loop since valid input was provided
     else:
-        print("Length must be a digit \n")
+        print("Length must be a digit \n")  # Prompt the user for valid input
